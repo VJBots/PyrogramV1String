bbd598d02d23af815891b06bc0f21661e7ba43de
VJBots <122880036+VJBots@users.noreply.github.com>
2023-08-30 17:47:30 +0530

    Update TechVJ.py

--- TechVJ.py
+++ TechVJ.py
@@ -6,10 +6,10 @@ api_id = 19181985
 api_hash = "a2b23ca3a1c9b5dab4bf42dda7de4e79"
 
 try:
-   os.remove("aditya.session")
+   os.remove("techvj.session")
 except:
      pass
-with Client("aditya", api_id=api_id, api_hash=api_hash) as app:
+with Client("techvj", api_id=api_id, api_hash=api_hash) as app:
     session = f"**🥀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 » 𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 💞**\n\n`{app.export_session_string()}`\n\n**💥 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲: [VJ Server](https://t.me/VJ_Botz) ✨**"
     app.send_message("me", session, disable_web_page_preview=True)
     try:
@@ -20,5 +20,5 @@ with Client("aditya", api_id=api_id, api_hash=api_hash) as app:
     except:
         pass
     print(f"✅ String Session Has 🌟 Been Sent\nTo Your 🔥 Saved Message ✨ ...")
-    os.remove("aditya.session")
+    os.remove("techvj.session")
 
